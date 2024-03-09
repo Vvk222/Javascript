c669b465cd131dd6926c05bf1eea1dd5f96eeb98
Vivek Khatri <100620558+Vvk222@users.noreply.github.com>
2024-03-09 11:25:58 +0000

    Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 236527;
+let accountEmail = "vkhatri9326@gmail.com";
+var accountPassword = "287648776982";
+accountCity = "Ahmedabad";
+let accountState 
+
+// accountId = 63547829 // not allowed
+// console.log(accountId);
+accountEmail = "vivekbkhatri123@gmail.com";
+accountPassword = "72584628264";
+accountCity = "Bengaluru";
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
